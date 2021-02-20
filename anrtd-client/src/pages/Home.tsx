0fc3f865e40501c5d4df115f2b401fe192a64bfc
MJeorrett <matthew@clearskylogic.com>
2021-02-20 18:24:57 +0000

    stlying

--- anrtd-client/src/pages/Home.tsx
+++ anrtd-client/src/pages/Home.tsx
@@ -38,8 +38,8 @@ const HomePage: React.FC<HomePageProps> = () => {
 
     return (
         <>
-            <h1>Welcome to Asp.Net React ToDos</h1>
-            <h3>The project layout, packages and boilerplate I like to use for Asp.Net Core + React + EF Core projects.</h3>
+            <Typography variant="h2" component="h1" gutterBottom>Welcome to Asp.Net React ToDos</Typography>
+            <Typography variant="h5" gutterBottom>The an example project using the layout, packages and boilerplate I like to use for Asp.Net Core + React + EF Core projects.</Typography>
 
             <Typography variant="h3">Create ToDo</Typography>
             <CreateToDoForm

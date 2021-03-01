29c2fb62675b8036e8e5b739ede8c2b44aaefb95
MJeorrett <matthew@clearskylogic.com>
2021-03-01 19:26:01 +0000

    #13 add toDo status FK constraint

---
+++ AnrtdApi/Anrtd.Infrastructure/Migrations/20210301191152_AddToDoStatusFK.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Anrtd.Infrastructure.Migrations
+{
+    public partial class AddToDoStatusFK : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_ToDo_Status",
+                table: "ToDo",
+                column: "Status");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ToDo_ToDoStatuses_Status",
+                table: "ToDo",
+                column: "Status",
+                principalTable: "ToDoStatuses",
+                principalColumn: "ToDoStatusId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ToDo_ToDoStatuses_Status",
+                table: "ToDo");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ToDo_Status",
+                table: "ToDo");
+        }
+    }
+}

e2ce5e33acf4472451f63051e7785aa7d19c237b
MJeorrett <matthew@clearskylogic.com>
2021-03-04 23:15:57 +0000

    move status filter to right

--- anrtd-client/src/pages/ToDos.tsx
+++ anrtd-client/src/pages/ToDos.tsx
@@ -1,12 +1,19 @@
+import { useTheme } from '@material-ui/core';
 import ToDoStatusFilter from '../components-todos/StatusFilter';
 import ToDosTable from '../components-todos/ToDosTable';
 import AppPageHeading from '../components/AppPageHeading';
 
 const ToDosPage: React.FC = () => {
+    const theme = useTheme();
     return (
         <>
             <AppPageHeading>ToDos</AppPageHeading>
-            <div>
+            <div
+                style={{
+                    display: 'flex',
+                    justifyContent: 'flex-end',
+                    marginRight: theme.spacing(1),
+                }}>
                 <ToDoStatusFilter />
             </div>
             <ToDosTable />
